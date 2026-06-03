8faff878f9f6c48d9aff3e04b9fac308ded7285b
Thanu <thanusigashrij@gmail.com>
2026-06-03 12:57:16 +0530

    Added task5 file

---
+++ task5.py
@@ -0,0 +1,16 @@
+attempt = 0
+
+while attempt < 3:
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    if username.isalpha() and password.isdigit():
+        print("Valid Input")
+        print("Registration done")
+        break
+    else:
+        print("Invalid Input")
+        attempt += 1
+
+if attempt == 3:
+    print("Account Locked")
